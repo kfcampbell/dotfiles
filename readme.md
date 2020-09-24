102bbf9cffd69a29412617e46ad66e4660b0640f
kfcampbell <kfcampbell@github.com>
2020-09-24 15:06:05 -0700

    Readme updates; copy over dotfiles

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Dotfiles
 
 This is at least one dotfile I'm storing here for convenience. Move along, nothing to see here...
+
+## Note to Future Keegan
+
+Make sure to try running `vim` and then `:PlugInstall` if the scripted installation doesn't do the trick.
--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ git config --global user.email "kfcampbell@github.com"
 ln -s ~/.dotfiles/.bashrc ~/
 ln -s ~/.dotfiles/.vimrc ~/
 
+# just in case?
+cp ~/.dotfiles/.bashrc ~/
+cp ~/.dotfiles/.vimrc ~/
+
 # try installing plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall --sync' +qall
