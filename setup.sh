a246defba548858ad9a0b1416f98b22e10edc7a0
kfcampbell <kfcampbell@github.com>
2020-09-24 14:06:30 -0700

    Add .vimrc and setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env/bash
+
+git config --global user.name "Keegan Campbell"
+git config --global user.email "kfcampbell@github.com"
+
+ln -s ~/.dotfiles/.bashrc ~/
+ln -s ~/.dotfiles/.vimrc ~/
+
+# try installing plugins
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +'PlugInstall --sync' +qall
